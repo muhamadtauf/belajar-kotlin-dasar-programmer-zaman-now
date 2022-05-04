2a42bf0d36da6be53f8324991eaec6cf72628bbc
Muhammad Taufik <muhamadtauf@gmail.com>
2022-05-04 11:40:27 +0700

    add operasi perbandingan, episode 11

---
+++ src/main/kotlin/OperasiPerbandingan.kt
@@ -0,0 +1,17 @@
+fun main() {
+    val a = 100
+    val b = 200
+
+    val result: Boolean = a > b
+    println(result)
+
+    println(100 >= 100)
+    println(100 <= 100)
+
+    println("eko" >= "eko")
+    println("eko" > "eko")
+    println("a" > "b")
+    println("a" < "b")
+    println("a" == "b")
+    println("a" != "b")
+}
